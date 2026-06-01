1fb08538794b08bf3072ec2de8e8b0cfad18e0b8
Aditya Pratap Singh <adityapratapsingh728@gmail.com>
2026-06-01 23:03:45 +0530

    Enhance build script to create Django superuser
    Added script to create a Django superuser if it doesn't exist.

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
 set -o errexit
+
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+import os
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', '')
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print('Superuser created successfully')
+else:
+    print('Superuser already exists, skipping')
+"
